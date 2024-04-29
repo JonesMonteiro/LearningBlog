13c57562c52f196f66e5922fe113458254847bab
JonesMonteiro <167978252+JonesMonteiro@users.noreply.github.com>
2024-04-29 16:58:59 +0100

    Redirects to the right page

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/posts', [PostController::class, 'index'])->name('post.index');
 });
 
 Route::resource('post', PostController::class)
